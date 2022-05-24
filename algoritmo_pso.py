91b2d3823a3b02874612510ceaf7ff1adfdf7d92
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2022-05-24 14:05:50 -0300

    atualização do algoritmo (não convergindo)

--- algoritmo_pso.py
+++ algoritmo_pso.py
@@ -3,24 +3,39 @@ import pandas as pd
 import random as rd
 import time
 
+
 class Particula(object):
-    def __init__(self, dimencao: int, inicializarPos: tuple = (0,1)) -> None:
+    def __init__(self, dimencao: int, inicializarPos: tuple = (0, 1)) -> None:
         self.posicao = np.random.uniform(inicializarPos[0], inicializarPos[1], dimencao)
-        self.velocidade = np.random.uniform(-abs(inicializarPos[1]-inicializarPos[0]),abs(inicializarPos[1]-inicializarPos[0]),dimencao)
+        self.velocidade = np.random.uniform(-abs(inicializarPos[1]-inicializarPos[0]), abs(inicializarPos[1]-inicializarPos[0]), dimencao)
         self.melhorPosicao = self.posicao
 
     def __str__(self) -> str:
-        return "Posicao: "+ str(self.posicao) + " | Velocidade: "+ str(self.velocidade) + " | Melhor posicao: " + str(self.melhorPosicao)
+        return "Posicao: " + str(self.posicao) + " | Velocidade: " + str(self.velocidade) + " | Melhor posicao: " + str(self.melhorPosicao)
 
 # Função a ser otimizada (minimizada) ====================================================================
 
-dim = 10 #dimenção D do problema (tamanho do vetor)
 
-shiftDataLoad = pd.read_table("shift_data_1.txt",delimiter='\s+', index_col=False, header=None)
-shiftData = shiftDataLoad.values.reshape((-1))[:dim]
+dim = 10 #dimenção D do problema (tamanho do vetor solução)
 
-matrixDataLoad = pd.read_table("M_1_D10.txt",delimiter='\s+', index_col=False, header=None)
-matrixData = matrixDataLoad.values
+# Shift Data
+shiftData1Load = pd.read_table("shift_data_1.txt",delimiter='\s+', index_col=False, header=None)
+shiftData2Load = pd.read_table("shift_data_2.txt",delimiter='\s+', index_col=False, header=None)
+shiftData1 = shiftData1Load.values.reshape((-1))[:dim]
+shiftData2 = shiftData2Load.values.reshape((-1))[:dim]
+
+# Matrix Data
+matrixData1Load = pd.read_table("M_1_D10.txt",delimiter='\s+', index_col=False, header=None)
+matrixData2Load = pd.read_table("M_2_D10.txt",delimiter='\s+', index_col=False, header=None)
+matrixData1 = matrixData1Load.values
+matrixData2 = matrixData2Load.values
+
+
+# Funções básicas
+
+def f2_bent_cigar__(solution=None):
+    return solution[0]**2 + 10**6 * sum(solution[1:]**2)
+
 
 def f1_elliptic__(solution=None):
     result = 0
@@ -28,92 +43,115 @@ def f1_elliptic__(solution=None):
         result += (10**6)**(i/(len(solution)-1)) * solution[i]**2
     return result
 
-def func_objetivo(solution=None, bias = 100):
-    z = np.dot(solution - shiftData, matrixData)
-    return f1_elliptic__(z) + bias
 
-# Froneiras do espaço de busca
+# F1
+# def func_objetivo(solution,bias=100):
+#     z = np.dot(solution- shiftData1, matrixData1)
+#     return f1_elliptic__(z) + bias
+# F2
+def func_objetivo(solution,bias=200):
+    z = np.dot(solution- shiftData2, matrixData2)
+    return f2_bent_cigar__(z) + bias
+
+
+# Fronteiras do espaço de busca
 limInferior = -100
 limSuperior = 100
 
 # Parâmetros do algoritmo
-w = 0.11 #inercia
-phiP = 1.6 #coeficiente cognitivo
-phiG = 2.1 #coeficiente social 
-numIndividuos = 50
-maxIter = 1000*dim
+w = 0.729  # inercia
+phiP = 1.49445  # coeficiente cognitivo
+phiG = 1.49445  # coeficiente social
+numParticulas = 50
+maxIter = 10000
+# Vmax = np.inf
 Vmax = 100
-repeticoes = 1
+repeticoes = 30
+
+wInicial = 0.6
+wFinal = 0.1
+
 inicio = time.time()
 
 solRepeticoes = []
 for repeticao in range(repeticoes):
-  print("Repetição ",repeticao+1)
 
-  # Inicializar População  =================================================================================
-  populacao = [Particula(dimencao=dim, inicializarPos=(limInferior,limSuperior)) for x in range(numIndividuos)]
-  # Inicializar melhor partícula
-  melhorParticula = None
-  for particula in populacao:
-      melhorParticula = particula
-      for vizinhos in populacao:
-          if func_objetivo(vizinhos.melhorPosicao) < func_objetivo(melhorParticula.posicao):
-              melhorParticula = vizinhos
-
-  # Loop Principal =========================================================================================
-  melhoresSolucoes = []
-  iteracao = 0
-  criterioParadaFlag = False
-  while not criterioParadaFlag:
-    iteracao+=1
-    # Critério de Parada ----------------------------------------------------------------------------
-    if (abs(200 - func_objetivo(melhorParticula.posicao)) <= 10**(-8)) or iteracao >= maxIter :
-      criterioParadaFlag = True
-    else:
-      # Atualizar população ---------------------------------------------------------------------------
-      for particula in populacao:
-
-        # atualizar melhor posições de cada indivíduo
-        if func_objetivo(particula.posicao) < func_objetivo(particula.melhorPosicao):
-          particula.melhorPosicao = particula.posicao
-
-        # Atualizar melhor Partícula 
+    # Inicializar População  =================================================================================
+    populacao = [Particula(dimencao=dim, inicializarPos=(limInferior, limSuperior)) for x in range(numParticulas)]
+    # Inicializar melhor partícula
+    melhorParticulaIndex = 0
+    for particula in range(numParticulas):
         melhorParticula = particula
-        # Checar se melhor partícula é de fato a melhor partícula
-        for vizinhos in populacao:
-          if func_objetivo(vizinhos.posicao) < func_objetivo(melhorParticula.posicao):
-            melhorParticula = vizinhos
+        for vizinhos in range(numParticulas):
+            if func_objetivo(populacao[vizinhos].posicao) < func_objetivo(populacao[melhorParticulaIndex].posicao):
+                melhorParticulaIndex = vizinhos
+    melhorFitness = func_objetivo(populacao[melhorParticulaIndex].posicao)
+    melhorPos = populacao[melhorParticulaIndex].melhorPosicao
 
-        # Atualizar velocidade e posição
-        for componente in range(dim):
-          rp = rd.uniform(0,1)
-          rg = rd.uniform(0,1)
-          #rp = 1
-          #rg = 1
-          particula.velocidade[componente] = (w* particula.velocidade[componente] 
-                                              + phiP*rp*(particula.melhorPosicao[componente] - particula.posicao[componente]) 
-                                              + phiG*rg*(melhorParticula.posicao[componente] - particula.posicao[componente]))
-          # Verificar intervalo (-Vmax,Vmax)
-          if particula.velocidade[componente] > Vmax:
-            particula.velocidade[componente] = Vmax
-          if particula.velocidade[componente] < -Vmax:
-            particula.velocidade[componente] = -Vmax
-        particula.posicao += particula.velocidade
+    # Loop Principal =========================================================================================
+    iteracao = 0
+    criterioParadaFlag = False
+    while not criterioParadaFlag:
 
-      print("iteração ",iteracao)
-    melhoresSolucoes.append(melhorParticula.posicao)
-  solRepeticoes.append(melhoresSolucoes[-1])
+        # Critério de Parada ----------------------------------------------------------------------------
+        if (abs(200 - melhorFitness) < 10 ** (-8)) or iteracao >= maxIter:
+            criterioParadaFlag = True
+        else:
+            # Atualizar população ---------------------------------------------------------------------------
+            wt = (wInicial - wFinal) * ((maxIter - iteracao) / maxIter) + wFinal  # Decremento linear de W
+            iteracao += 1
+
+            # Atualizar melhor posições de cada indivíduo
+            for particula in range(numParticulas):
+                if func_objetivo(populacao[particula].posicao) < func_objetivo(populacao[particula].melhorPosicao):
+                    populacao[particula].melhorPosicao = populacao[particula].posicao
+
+            # Atualizar melhor particula global
+            for vizinhos in range(numParticulas):
+                if func_objetivo(populacao[vizinhos].melhorPosicao) < melhorFitness:
+                    melhorParticulaIndex = vizinhos
+                    melhorFitness = func_objetivo(populacao[melhorParticulaIndex].melhorPosicao)
+                    melhorPos = populacao[melhorParticulaIndex].melhorPosicao
+
+            # Atualizar velocidade e posição
+            for particula in range(numParticulas):
+                for componente in range(dim):
+                    rp = rd.uniform(0, 1)
+                    rg = rd.uniform(0, 1)
+                    # rp = 1
+                    # rg = 1
+                    populacao[particula].velocidade[componente] = (wt * populacao[particula].velocidade[componente]
+                                                                   + phiP * rp * (populacao[particula].melhorPosicao[
+                                                                                      componente] -
+                                                                                  populacao[particula].posicao[
+                                                                                      componente])
+                                                                   + phiG * rg * (melhorPos[componente] -
+                                                                                  populacao[particula].posicao[
+                                                                                      componente]))
+                    # Verificar intervalo (-Vmax,Vmax)
+                    if populacao[particula].velocidade[componente] > Vmax:
+                        populacao[particula].velocidade[componente] = Vmax
+                    elif populacao[particula].velocidade[componente] < - Vmax:
+                        populacao[particula].velocidade[componente] = - Vmax
+                    populacao[particula].posicao[componente] += populacao[particula].velocidade[componente]
+                    # if populacao[particula].posicao[componente] > limSuperior:
+                    #     populacao[particula].posicao[componente] = limSuperior
+                    # elif populacao[particula].posicao[componente] < limInferior:
+                    #     populacao[particula].posicao[componente] = limInferior
+            # if iteracao % 100 == 0:
+            # print("Repetição: {} | Iteração: {} | Melhor FItness: {}".format(repeticao + 1, iteracao, melhorFitness))
+
+            # print("{}".format(iteracao))
+    solRepeticoes.append(melhorFitness)
+    print("Repetição: {} | Iteração Máxima: {} | Melhor FItness: {}".format(repeticao + 1, iteracao, melhorFitness))
 fim = time.time()
 
 # Imprimir resultados =======================================================
-print("Média das repetições:",np.mean([func_objetivo(x) for x in solRepeticoes]))
-print("Desvio padrão das repetições:",np.std([func_objetivo(x) for x in solRepeticoes]))
+print("Média das repetições: {:.8f}\n".format(np.mean(solRepeticoes)), end="")
+print("Desvio padrão das repetições: {:.2f}\n".format(np.std(solRepeticoes)), end="")
 
 # Imprimir tempo de execução =================================================
-print()
-if (fim-inicio<=60):
-    print(fim - inicio,"segundos")
+if (fim - inicio <= 60):
+    print(fim - inicio, "segundos")
 else:
-    print((fim - inicio)/60,"minutos")
-
-
+    print((fim - inicio) / 60, "minutos")
